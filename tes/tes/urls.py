0b6661b7a14078f57bbbf0fdb5441848dbade3f3
vigneshp9898 <69506038+vigneshp9898@users.noreply.github.com>
2020-08-17 21:37:48 +0530

    error for signup

--- tes/tes/urls.py
+++ tes/tes/urls.py
@@ -20,6 +20,6 @@ urlpatterns = [
     path('', include('project.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('accounts/',include('accounts.urls')),
+    #path('accounts/',include('accounts.urls')),
 
 ]
